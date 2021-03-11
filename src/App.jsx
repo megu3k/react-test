75968372a66a49f265018097268411ec27d57b4f
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-11 12:26:05 +0900

    useEffect unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
 
   return (
     <>
@@ -13,7 +20,10 @@ const App = () => {
       <ColofulMessage color="blue">お元気ですか?</ColofulMessage>
       <ColofulMessage color="pink">元気です!</ColofulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( *´艸｀)</p>}
     </>
   );
 };
